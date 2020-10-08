a8c7e4c904f8fda5e19a469ca0d66de87f39a035
ZinkPavel <tsynk.p@gmail.com>
2020-10-08 10:27:11 +0700

    Changed semantics check func declaration

--- src/Semantics/Semantics.cpp
+++ src/Semantics/Semantics.cpp
@@ -46,9 +46,24 @@ Block::Block (Expression& expr) {
     } else throw std::runtime_error("Block constructor: wrong expression type. Expected FuncDeclarationExpression.");
 }
 
+void Block::multipleDeclarationCheck () {
+    for (auto& var : vars) {
+        size_t count = std::count_if(vars.begin(), vars.end(), [var](Var& _var) -> bool {
+            return var.litteral == _var.litteral;
+        });
+        if (count > 1) throw std::runtime_error("Multiple declaration error");
+    }
+}
+
+/* Operators */
+
 std::ostream& operator << (std::ostream& os, Var& var) {
     os << "<" << var.row << ":" << var.col << "> " << var.litteral << " " << var.dataType << " = " << var.value;
     return os;
+}
+
+bool operator == (Var& lhs, Var& rhs) {
+    return tie(lhs.row, lhs.col, lhs.litteral, lhs.dataType, lhs.value) == tie(rhs.row, rhs.col, rhs.litteral, rhs.dataType, rhs.value);
 }
 
 void semanticsAnalysis (const std::vector<std::shared_ptr<Expression>>& exprs) {
@@ -66,13 +81,18 @@ void semanticsAnalysis (const std::vector<std::shared_ptr<Expression>>& exprs) {
         case 5: ifExprs.push_back(expr); break; // ...
         case 6: whileExprs.push_back(expr); break; // ...
         case 7: funcDeclareExprs.push_back(expr); break; // OK
-        case 8: varDefinitionExprs.push_back(expr); break; // OK
-        case 9: varDeclarationExprs.push_back(expr); break; // OK
+        case 8: varDefinitionExprs.push_back(expr); break; // ...
+        case 9: varDeclarationExprs.push_back(expr); break; // ...
         case 10: funcCallExprs.push_back(expr); break; // ...
         default: break;
         }
     }
 
+    for (auto& expr : funcDeclareExprs) {
+        blocks.push_back({*expr});
+        blocks.back().multipleDeclarationCheck();
+    }
+    // std::cout << blocks.size() << std::endl;
     // for (auto& expr : funcDeclareExprs) blocksByVars.push_back({expr, {}});
 
     // semCheckFuncDeclare(blocksByVars);
@@ -85,31 +105,6 @@ void semanticsAnalysis (const std::vector<std::shared_ptr<Expression>>& exprs) {
         ss << "BLOCK\n" << Join(block->actualTokenSeq, ' ') << '\n' << Join(vars, '\n') << "\n\n";
     }
     std::cout << ss.str() << std::endl; */
-}
-
-void semCheckFuncDeclare (std::vector<std::pair<std::shared_ptr<Expression>, std::vector<Token>>>& blocksByVars) {
-    for (auto& [block, vars] : blocksByVars) {
-        if (block->type != 7) continue;
-        
-        std::string strTokens = block.get()->getStrTokensType(' ');
-        std::regex regex("L_PAREN (identifier (COMMA identifier)?\\s?(int|string|bool)) R_PAREN");
-
-        if (!std::regex_search(strTokens, regex)) continue; // has not args
-
-        std::string typeArgs;
-        auto it = std::find_if(block->actualTokenSeq.rbegin(), block->actualTokenSeq.rend(), [](Token& token) -> bool {
-            return token.type == "R_PAREN";
-        });
-
-        typeArgs = (it + 1)->type;
-        for (; it->type != "L_PAREN"; it++) {
-            if (it->type == "identifier") {
-                it->dataType = typeArgs;
-                vars.push_back(*it);
-            }
-        }
-    }
-    // semCheckMultipleDeclaration(blocksByVars);
 }
 
 // void semCheckVarDeclaration (std::vector<std::shared_ptr<Expression>> varDeclarationExprs, std::vector<std::pair<std::shared_ptr<Expression>, std::vector<Token>>>& blocksByVars) {
@@ -175,13 +170,3 @@ void semCheckFuncDeclare (std::vector<std::pair<std::shared_ptr<Expression>, std
 //     } */
 // }
 
-// void semCheckMultipleDeclaration (std::vector<std::pair<std::shared_ptr<Expression>, std::vector<Token>>>& blocksByVars) {
-//     for (auto& blockByVar : blocksByVars) {
-//         for (auto& var : blockByVar.second) {
-//             size_t result = std::count_if(blockByVar.second.begin(), blockByVar.second.end(), [var](Token& token) -> bool {
-//                 return token.litteral == var.litteral;
-//             });
-//             if (result > 1) throw std::runtime_error("Multiple declaration error");
-//         }
-//     }  
-// }
--- src/Semantics/Semantics.h
+++ src/Semantics/Semantics.h
@@ -21,15 +21,17 @@ public:
     std::vector<Var> vars;
 
     Block (Expression& expr);
+
+    void multipleDeclarationCheck ();
 };
 
 /* Operaotors */
 
 std::ostream& operator << (std::ostream& os, Var& var);
+bool operator == (Var& lhs, Var& rhs);
 
 void semanticsAnalysis (const std::vector<std::shared_ptr<Expression>>& exprs);
 
-void semCheckFuncDeclare (std::vector<std::pair<std::shared_ptr<Expression>, std::vector<Token>>>& blocksByVars);
 void semCheckVarDeclaration (std::vector<std::shared_ptr<Expression>> varDeclarationExprs, std::vector<std::pair<std::shared_ptr<Expression>, std::vector<Token>>>& blocksByVars);
 void semCheckVarDefinition (std::vector<std::shared_ptr<Expression>> varDefinitionExprs, std::vector<std::pair<std::shared_ptr<Expression>, std::vector<Token>>>& blocksByVars);
 void semCheckReturn (std::vector<std::shared_ptr<Expression>> returnExprs, std::vector<std::pair<std::shared_ptr<Expression>, std::vector<Token>>>& blocksByVars);
