51c2626162f5ddb533bf10708d0049a5689ff453
ZinkPavel <tsynk.p@gmail.com>
2020-10-08 22:48:08 +0700

    Fix make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p build/Compiler build/Parser build/Parser/AST-Tree build/Tests build/Tools
+mkdir -p bin build build/Semantics build/Compiler build/Parser build/Parser/AST-Tree build/Tests build/Tools
 
 make
